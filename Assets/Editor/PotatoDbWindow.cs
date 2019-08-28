68faac6afd4b76afc3f1e2877a329584f88f1f40
Terra Lauterbach <terra.lauterbach@gmail.com>
2019-08-28 18:52:15 -0500

    added base unity project

---
+++ Assets/Editor/PotatoDbWindow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace PotatoDb.Editor {
+
+	/// <summary>
+	/// Editor window class for editing your PotatoDb content
+	/// </summary>
+	public class PotatoDbWindow : EditorWindow {
+
+
+		[MenuItem("Potato Db", menuItem = "Window/PotatoDb")]
+		public static void ShowWindow() {
+
+			PotatoDbWindow window = GetWindow<PotatoDbWindow>(false, "Potato Db", true);
+			window.minSize = new Vector2(400f, 300f);
+			window.ShowTab();
+		}
+
+	}
+
+}
+
+
